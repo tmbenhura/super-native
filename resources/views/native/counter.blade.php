a70cab4313a8900dbed6e2aadc59aba89f0ad985
Shane Rosenthal <srosenthal82@gmail.com>
2026-07-05 20:22:33 -0400

    Updates

--- resources/views/native/counter.blade.php
+++ resources/views/native/counter.blade.php
@@ -1,19 +1,14 @@
 <column class="w-full h-full items-center justify-center bg-theme-surface-variant gap-8">
     <text @press="testCamera" class="text-[120] font-bold text-theme-on-surface-variant">
-        {{ $count * 8}}
+        {{ $count }}
     </text>
 
     <row class="gap-8 ">
-        <native:icon @longPress="increment" @press="decrement" :size="40"
-                     a11y-label="Decrement"
-                     class="text-center text-white px-8 py-4 shadow rounded bg-orange-500"
-                     :android="App\Icons\Android::ArrowDropDown"
-                     :ios="App\Icons\Ios::ChevronDown"
-        />
-        <native:icon @press="increment" :size="40" a11y-label="Increment"
-                     class="text-center text-white px-8 py-4 shadow rounded bg-blue-500"
-                     :android="App\Icons\AndroidOutlined::ArrowDropUp"
-                     :ios="App\Icons\Ios::ChevronUp"
-        />
+        <pressable @press="decrement" class="text-center text-white px-8 py-4 shadow rounded bg-orange-500">
+            <native:icon class="text-white" :size="40" a11y-label="Decrement" :android="App\Icons\Android::ArrowDropDown" :ios="App\Icons\Ios::ChevronDown"/>
+        </pressable>
+        <pressable @press="increment" class="text-center text-white px-8 py-4 shadow rounded bg-blue-500">
+            <native:icon class="text-white" :size="40" a11y-label="Decrement" :android="App\Icons\Android::ArrowDropUp" :ios="App\Icons\Ios::ChevronUp"/>
+        </pressable>
     </row>
 </column>
